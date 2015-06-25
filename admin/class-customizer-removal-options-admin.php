c840cf0ab7cb02fbfcadaea3e2234ceef517d792
Jesse Petersen <jesse@petersenmediagroup.com>
2015-06-25 15:59:14 -0400

    Getting started with structure and getting some code going

---
+++ admin/class-customizer-removal-options-admin.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Customizer_Removal_Options
+ * @subpackage Customizer_Removal_Options/admin
+ */
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the admin-specific stylesheet and JavaScript.
+ *
+ * @package    Customizer_Removal_Options
+ * @subpackage Customizer_Removal_Options/admin
+ * @author     Jesse Petersen, Andy Wilkerson
+ */
+class Customizer_Removal_Options_Admin {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $plugin_name    The ID of this plugin.
+	 */
+	private $plugin_name;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $plugin_name       The name of this plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	public function __construct( $plugin_name, $version ) {
+
+		$this->customizer_removal_options = $plugin_name;
+		$this->1.0.0 = $version;
+
+	}
+
+}
